32d18778ec1333bbf4c0d46fbc804a7e7d26c9c9
natureyoghurt <natureyoghurt@gmail.com>
2018-11-20 07:15:23 +0100

    intermediate

--- person-editor/src/app/app.component.ts
+++ person-editor/src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'person-editor';
+  // title = 'person-editor';
+  title = 'hello person-editor';
 }
